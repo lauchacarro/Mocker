fd54150c9a4c2648c97c5c40d5f897bbf8ccbd81
lautaro <lautaroecarro@gmail.com>
2019-09-08 23:03:44 -0300

    FileStream to string

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,9 +14,28 @@ namespace Mocker.Controllers
     public class FilesController : ControllerBase
     {
         [HttpPost]
-        public async Task<IActionResult> Create()
+        public async Task<IActionResult> Create(List<IFormFile> files)
         {
-             
+            long size = files.Sum(f => f.Length);
+
+            var filePath = Path.GetTempFileName();
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                        byte[] bytes = new byte[stream.Length];
+                        stream.Position = 0;
+                        stream.Read(bytes, 0, (int)stream.Length);
+                        string data = Encoding.ASCII.GetString(bytes);
+                    }
+                }
+            }
+
+            return Ok(new { count = files.Count, size, filePath });
         }
     }
 }
